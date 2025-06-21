1696ee6229d4082cfd41aa4ea46a93e129934522
Alain Guillon <zyrass@outlook.fr>
2025-06-22 00:45:41 +0200

    fix: locales
    Suppression de 'en' dans la locales du fichier de configuration.

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -43,7 +43,7 @@ const config: Config = {
     // may want to replace "en" with "zh-Hans".
     i18n: {
         defaultLocale: 'fr',
-        locales: ['en', 'fr'],
+        locales: ['fr'],
     },
 
     // https://prismjs.com/#supported-languages
